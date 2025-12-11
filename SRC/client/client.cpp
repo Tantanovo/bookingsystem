8918a9665b3c7c59324c457457d12d4ed1c05868
Tantanovo <1348215719@qq.com>
2025-12-12 00:47:33 +0800

    c/s初始化

--- SRC/client/client.cpp
+++ SRC/client/client.cpp
@@ -1,4 +1,12 @@
 #include"client.hpp"
+
+
+
 int main(){
-    
+    connect_client client;
+    if(!client.init_client()){
+        perror("init client err");
+        return -1;
+    }
+    return 0;
 }
